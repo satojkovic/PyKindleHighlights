db82c933dd9604ebb59757a9c0575818d9e87e7d
satojkovic <satojkovic@gmail.com>
2014-02-24 23:14:54 +0900

    change main.py

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 from pit import Pit
 from kindle.highlights import PyKindleHighlights
 
+
 def main():
-    config = Pit.get('amazon.co.jp', {'require': {'email': 'email', 'password': 'password'}})
+    config = Pit.get('amazon.co.jp',
+                     {'require': {'email': 'email', 'password': 'password'}})
 
     kindle = PyKindleHighlights(config['email'], config['password'])
 
     for highlight in kindle.highlights:
-        print '[title] %s' %highlight.title
-        print '[author] %s' %highlight.author
+        print '[title] %s' % highlight.title
+        print '[author] %s' % highlight.author
         for i in range(len(highlight.text)):
-            print '(%d) %s' %(i+1, highlight.text[i])
-    
+            print '(%d) %s' % (i+1, highlight.text[i])
+
 if __name__ == '__main__':
     main()
